8eb3b6f3d04183b105c03e76697923dcbbfb401d
kai-dg <km.commissions@gmail.com>
2019-04-03 09:57:28 +0000

    adding most of task 6 req

--- models/city.py
+++ models/city.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+import os
 
 
 class City(BaseModel):
@@ -9,5 +11,11 @@ class City(BaseModel):
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
+    else:
+        state_id = ""
+        name = ""
