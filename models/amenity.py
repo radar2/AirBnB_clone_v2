f46c32630b6ddd6ad2d0844b2e154e4fc35c2352
kai-dg <km.commissions@gmail.com>
2019-04-04 03:27:31 +0000

    added Base inheritance

--- models/amenity.py
+++ models/amenity.py
@@ -5,14 +5,14 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
     __tablename__ = "amenities"
 
-    if os.getenv("HBNB_TYPE_STORAGE") = "db":
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary="place_amenity")
     else:
