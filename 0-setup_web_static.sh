29d85003215ffd07072477478bcac9c6d7bfb1af
kai-dg <km.commissions@gmail.com>
2019-04-09 14:05:17 +0000

    added sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # setup for deployment
-apt-get -y update
-apt-get -y install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
 SRC="/etc/nginx/sites-available/default"
-STATIC="\\\tlocation /hbnb_static/ {\n\t\t alias /data/web_static_current/;\n\t}\n"
+STATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static_current/;\n\t}\n"
 sudo mkdir -p "/data/web_static/releases/test/"
 sudo mkdir "/data/web_static/shared/"
 echo "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
