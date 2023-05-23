c9c22dbef90523c3be4cdfa46f503bd3d5dc3674
radar2 <securkydroity@gmail.com>
2023-05-23 12:58:42 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Flask Web framework
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Hello route function.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Hbnb Function.
+    """
+    return "HBNB"
+
+
+if __name__=="__main__":
+    app.run()
