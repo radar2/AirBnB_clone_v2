3cba6db335c24a3b9da765de9174458505abaf69
radar2 <securkydroity@gmail.com>
2023-05-23 12:44:35 +0000

    Fix 5

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,12 +7,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strick_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_route():
-    """hello route function
+    """Hello route function.
     """
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
+    print(hello_route.__doc__)
     app.run()
