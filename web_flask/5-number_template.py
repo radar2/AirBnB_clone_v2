59aa58909ede8e290c5e1bbba9e1cae6a6fd5ca3
radar2 <securkydroity@gmail.com>
2023-05-23 14:18:06 +0000

    number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Flask Web framework
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Hello route function.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Hbnb Function.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """C is fun Function
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Python is Cool Function.
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_number(n):
+    """N is a number.
+    """
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Number template.
+    """
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run()
