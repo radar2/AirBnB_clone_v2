7fb091cc9d587cbba9c4e87b8a7f680c2c3004e5
radar2 <securkydroity@gmail.com>
2023-05-23 10:18:57 +0000

    fix 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Flask"""
-from flask import Flask, request
+from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_route():
     """hello route"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run()
